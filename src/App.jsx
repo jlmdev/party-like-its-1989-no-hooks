4b256f75f2288947db1d0d9f2caa4115a1a6e273
Josh Mann <jlmann72@gmail.com>
2020-09-23 17:15:08 -0400

    Explorer mode complete

--- src/App.jsx
+++ src/App.jsx
@@ -1,38 +1,6 @@
 import React, { Component } from 'react'
-// import movieData from './components/batman.json'
-// import movieData1989 from './components/MoviesFrom1989.json'
 
-// class Movie extends Component {
-//   state = movieData
-
-//   render() {
-//     return (
-//       <div className="movie">
-//         <ul>
-//           <li className="headline">
-//             <h3 className="title">{this.state.title}</h3>
-//             <img
-//               src={`https://image.tmdb.org/t/p/w92${this.state.poster_path}`}
-//               alt="Poster Image"
-//             />
-//           </li>
-//           <li></li>
-//           <li className="backdrop">
-//             <img
-//               src={`https://image.tmdb.org/t/p/w500${this.state.backdrop_path}`}
-//               alt="Backdrop Image"
-//             />
-//           </li>
-//           <li className="overview">
-//             <p>{this.state.overview}</p>
-//           </li>
-//         </ul>
-//       </div>
-//     )
-//   }
-// }
-
-class App extends Component {
+class Movie extends Component {
   state = {
     results: [],
   }
@@ -59,14 +27,14 @@ class App extends Component {
                   <h3 className="title">{movie.title}</h3>
                   <img
                     src={`https://image.tmdb.org/t/p/w92${movie.poster_path}`}
-                    alt="Poster Image"
+                    alt={movie.title}
                   />
                 </li>
                 <li></li>
                 <li className="backdrop">
                   <img
                     src={`https://image.tmdb.org/t/p/w500${movie.backdrop_path}`}
-                    alt="Backdrop Image"
+                    alt={movie.title}
                   />
                 </li>
                 <li className="overview">
@@ -78,6 +46,12 @@ class App extends Component {
         </main>
       </>
     )
+  }
+}
+
+class App extends Component {
+  render() {
+    return <Movie />
   }
 }
 

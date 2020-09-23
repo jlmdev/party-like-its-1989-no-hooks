2b1ccdddbb0c35c0c4313e9ce3dab3cb954568f8
Josh Mann <jlmann72@gmail.com>
2020-09-23 16:05:47 -0400

    Last save before attempting array

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,6 @@
 import React, { Component } from 'react'
 import movieData from './components/batman.json'
+import movieData1989 from './components/MoviesFrom1989.json'
 
 class Movie extends Component {
   state = movieData
@@ -23,15 +24,7 @@ class Movie extends Component {
             />
           </li>
           <li className="overview">
-            <p>
-              Having witnessed his parents' brutal murder as a child,
-              millionaire philanthropist Bruce Wayne fights crime in Gotham City
-              disguised as Batman, a costumed hero who strikes fear into the
-              hearts of villains. But when a deformed madman who calls himself
-              \"The Joker\" seizes control of Gotham's criminal underworld,
-              Batman must face his most ruthless nemesis ever while protecting
-              both his identity and his love interest, reporter Vicki Vale.
-            </p>
+            <p>{this.state.overview}</p>
           </li>
         </ul>
       </div>
@@ -46,10 +39,12 @@ class App extends Component {
   //   "video":false,
   //   "poster_path":
   //   "\/lH3dsbxA4wLalWKNYpVldX8zfPP.jpg",
-  //   "id":268,"adult":false,
+  //   "id":268,
+  //   "adult":false,
   //   "backdrop_path":"\/2va32apQP97gvUxaMnL5wYt4CRB.jpg",
   //   "original_language":"en",
-  //   "original_title":"Batman","genre_ids":[28,14],
+  //   "original_title":"Batman",
+  //   "genre_ids":[28,14],
   //   "title":"Batman",
   //   "vote_average":7.2,
   //   "overview":"Having witnessed his parents' brutal murder as a child, millionaire philanthropist Bruce Wayne fights crime in Gotham City disguised as Batman, a costumed hero who strikes fear into the hearts of villains. But when a deformed madman who calls himself \"The Joker\" seizes control of Gotham's criminal underworld, Batman must face his most ruthless nemesis ever while protecting both his identity and his love interest, reporter Vicki Vale.",

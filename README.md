73670df2bf4c2dd624a968ad905e73ef2da8dbdf
Josh Mann <jlmann72@gmail.com>
2020-09-23 15:46:48 -0400

    Styling done for movie class

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # Party Like Its 1989 No Hooks
+
+https://api.themoviedb.org/3/discover/movie?primary_release_year=1989&sort_by=popularity.desc&api_key=0d4f91e12a85ebe53cbee6c7603388e5
--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,71 @@
 import React, { Component } from 'react'
+import movieData from './components/batman.json'
+
+class Movie extends Component {
+  state = movieData
+
+  render() {
+    return (
+      <div className="movie">
+        <ul>
+          <li className="headline">
+            <h3 className="title">{this.state.title}</h3>
+            <img
+              src={`https://image.tmdb.org/t/p/w92${this.state.poster_path}`}
+              alt="Poster Image"
+            />
+          </li>
+          <li></li>
+          <li className="backdrop">
+            <img
+              src={`https://image.tmdb.org/t/p/w500${this.state.backdrop_path}`}
+              alt="Backdrop Image"
+            />
+          </li>
+          <li className="overview">
+            <p>
+              Having witnessed his parents' brutal murder as a child,
+              millionaire philanthropist Bruce Wayne fights crime in Gotham City
+              disguised as Batman, a costumed hero who strikes fear into the
+              hearts of villains. But when a deformed madman who calls himself
+              \"The Joker\" seizes control of Gotham's criminal underworld,
+              Batman must face his most ruthless nemesis ever while protecting
+              both his identity and his love interest, reporter Vicki Vale.
+            </p>
+          </li>
+        </ul>
+      </div>
+    )
+  }
+}
 
 class App extends Component {
+  // state = {
+  //   "popularity":42.011,
+  //   "vote_count":5011,
+  //   "video":false,
+  //   "poster_path":
+  //   "\/lH3dsbxA4wLalWKNYpVldX8zfPP.jpg",
+  //   "id":268,"adult":false,
+  //   "backdrop_path":"\/2va32apQP97gvUxaMnL5wYt4CRB.jpg",
+  //   "original_language":"en",
+  //   "original_title":"Batman","genre_ids":[28,14],
+  //   "title":"Batman",
+  //   "vote_average":7.2,
+  //   "overview":"Having witnessed his parents' brutal murder as a child, millionaire philanthropist Bruce Wayne fights crime in Gotham City disguised as Batman, a costumed hero who strikes fear into the hearts of villains. But when a deformed madman who calls himself \"The Joker\" seizes control of Gotham's criminal underworld, Batman must face his most ruthless nemesis ever while protecting both his identity and his love interest, reporter Vicki Vale.",
+  //   "release_date":"1989-06-23"}
+
   render() {
-    return <div>Hello, World!</div>
+    return (
+      <>
+        <header>
+          <h1>Party Like it's 1989!</h1>
+        </header>
+        <main>
+          <Movie />
+        </main>
+      </>
+    )
   }
 }
 

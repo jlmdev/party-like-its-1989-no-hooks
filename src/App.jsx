8c81f61962cb1ba747972fabf64967815ae5e9cd
Josh Mann <jlmann72@gmail.com>
2020-09-23 17:00:31 -0400

    responsive design working

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 import React, { Component } from 'react'
-import movieData from './components/batman.json'
-import movieData1989 from './components/MoviesFrom1989.json'
+// import movieData from './components/batman.json'
+// import movieData1989 from './components/MoviesFrom1989.json'
 
 // class Movie extends Component {
 //   state = movieData
@@ -33,7 +33,17 @@ import movieData1989 from './components/MoviesFrom1989.json'
 // }
 
 class App extends Component {
-  state = movieData1989
+  state = {
+    results: [],
+  }
+
+  async componentDidMount() {
+    const response = await fetch(
+      'https://api.themoviedb.org/3/discover/movie?primary_release_year=1989&sort_by=popularity.desc&api_key=0d4f91e12a85ebe53cbee6c7603388e5'
+    )
+    const apiJSON = await response.json()
+    this.setState(apiJSON)
+  }
 
   render() {
     return (
@@ -42,8 +52,8 @@ class App extends Component {
           <h1>Party Like it's 1989!</h1>
         </header>
         <main>
-          <div className="movie">
-            {this.state.results.map(movie => (
+          {this.state.results.map(movie => (
+            <div className="movie">
               <ul>
                 <li className="headline">
                   <h3 className="title">{movie.title}</h3>
@@ -63,8 +73,8 @@ class App extends Component {
                   <p>{movie.overview}</p>
                 </li>
               </ul>
-            ))}
-          </div>
+            </div>
+          ))}
         </main>
       </>
     )
